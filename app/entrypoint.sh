5bde223757165066429609935b26f158026337b3
Daffa Randika <daffa.randika.3@gmail.com>
2026-09-22 12:54:10 +0700

    fix path di entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -6,12 +6,12 @@ echo "Menjalankan portofolio untuk: $TARGET_MEMBER"
 
 rm -rf /usr/share/nginx/html/*
 
-if [ -d "/app/src/$TARGET_MEMBER/src" ]; then
-    cp -r "/app/src/$TARGET_MEMBER/src/"* /usr/share/nginx/html/
+if [ -d "/app/app/src/$TARGET_MEMBER/src" ]; then
+    cp -r "/app/app/src/$TARGET_MEMBER/src/"* /usr/share/nginx/html/
 else
-    echo "Folder /app/src/$TARGET_MEMBER/src tidak ditemukan!"
+    echo "Folder /app/app/src/$TARGET_MEMBER/src tidak ditemukan!"
 fi
 
-echo "halo.dek = { hostname: \"${TARGET_MEMBER}\" };" > /usr/share/nginx/html/config.js
+echo "halo dek, abang punya server bernama: \"${TARGET_MEMBER}\" ;" > /usr/share/nginx/html/config.js
 
 exec nginx -g 'daemon off;'
